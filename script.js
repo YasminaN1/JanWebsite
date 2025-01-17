632933a4196a3836ffaa6c8737bc8e8c975e36e3
YasminaN1 <526ynigmanova@frhsd.com>
2025-01-17 09:22:01 -0500

    Made my buttons work

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // ROCK PAPER SCISSORS
 
-const choices = { "coral", "seaweed", "claw"};
+const choices = ["coral", "seaweed", "claw"];
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
 const resultDisplay = document.getElementById("resultDisplay");
@@ -10,43 +10,42 @@ let playerScore = 0;
 let computerScore = 0;
 
 function playGame(playerChoice) {
+  console.log("playing");
+  const computerChoice = choices[Math.floor(Math.random() * 3)];
+  let result = "";
 
-    const computerChoice = choices[Math.floor(Math.random() * 3)];
-    let result = "";
-
-    if (playerChoice === computerChoice) {
-        result = "It's a tie!";
+  if (playerChoice == computerChoice) {
+    result = "It's a tie!";
+  } else {
+    switch (playerChoice) {
+      case "coral":
+        result = computerChoice === "seaweed" ? "You win!" : "You lose!";
+        break;
+      case "seaweed":
+        result = computerChoice === "coral" ? "You win!" : "You lose!";
+        break;
+      case "claw":
+        result = computerChoice === "seaweed" ? "You win!" : "You lose!";
+        break;
     }
-    else {
-        switch (playerChoice) {
-            case "coral":
-              result =  (computerChoice === "seaweed") ? "You win!" : "You lose!"
-              break;
-              case "seaweed":
-              result =  (computerChoice === "coral") ? "You win!" : "You lose!"
-              break;
-              case "claw":
-                result =  (computerChoice === "seaweed") ? "You win!" : "You lose!"
-                break;
-        }
-    }
+  }
+  console.log(result);
+  playerDisplay.textContent = `Player: ${playerChoice}`;
+  computerDisplay.textContent = `computer: ${computerChoice}`;
+  resultDisplay.textContent = result;
 
-    playerDisplay.textContent = `Player: ${playerChoice}`;
-    computerDisplay.textContent = `computer: ${computerChoice}`;
-    resultDisplay.textContent = result;
-// 15:08
-resultDisplay.classList.remove("greenText, redText");
+  resultDisplay.classList.remove("greenText, redText");
 
-    switch(result){
-        case "You win!":
-            resultDisplay.classList.add("greenText");
-            playerScore++;
-            playerScoreDisplay. textContent = playerScore;
-            break;
-            case "You lose!":
-                resultDisplay.classList.add("redText");
-                computerScore++;
-                computerScoreDisplay. textContent = computerScore;
-                break;
-    }
+  switch (result) {
+    case "You win!":
+      resultDisplay.classList.add("greenText");
+      playerScore++;
+      playerScoreDisplay.textContent = playerScore;
+      break;
+    case "You lose!":
+      resultDisplay.classList.add("redText");
+      computerScore++;
+      computerScoreDisplay.textContent = computerScore;
+      break;
+  }
 }
